321491d20d33bce6666b8456c7f38051bba15a50
lucasnunessg <lucasnunespacheco@gmail.com>
2023-03-17 17:24:50 -0300

    fazendo o requisito 3 e já comentando no codigo

---
+++ src/Pages/Settings.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Settings extends React.Component {
+  render() {
+    return (
+      <h1
+        data-testid="settings-title"
+      >
+        Configurações
+      </h1>
+    );
+  }
+}
+
+export default Settings;
+// so criei aqui pra ser renderizado no login
