6433f7743734acf8eb979a9a7b3353c7f0d9e7b6
pedror13 <pedrohenrique.rosemberg.phsr@gmail.com>
2023-03-22 07:07:45 -0300

    Lint

--- src/Pages/Feedback.js
+++ src/Pages/Feedback.js
@@ -3,35 +3,40 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
 
+feedbackMessage = () => {
+// pegar o valor de assertions como prop
+  // const magicNumber = 3;
+  // if (assertions < magicNumber) {
+  //   return 'Could be better...';
+  // }
+  // if (assertions >= magicNumber) {
+  //   return 'Well Done!';
+  // }
+};
+
 class Feedback extends React.Component {
-  const {
-    finalScore,
-    correctAnswers,
-  } = this.props;
   render() {
-    console.log(this.props.st);
     return (
       <div>
         <Header />
         <div>
           <h1>Placar</h1>
-          <span data-testid="feedback-total-score">{ finalScore }</span>
+          <span data-testid="feedback-total-score">[colocar o valor do score] </span>
           <h2>Acertos :</h2>
-          <span data-testid="feedback-total-question">{correctAnswers }</span>
+          <span data-testid="feedback-total-question">[colocar o valor assertions]</span>
         </div>
-
+        {/* <h2>{ this.feedbackMessage() }</h2> */}
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  finalScore: state.score,
-  correctAnswers: state.count,
+  ...state.player,
 });
 
 Feedback.propTypes = {
-  
+
 };
 
 export default connect(mapStateToProps)(Feedback);
